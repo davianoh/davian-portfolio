e90e1fe5bfddcb0bc70722140d56f7129685cb1e
davianoh <davianbenito4@gmail.com>
2024-11-12 17:34:43 +0700

    update project cards

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -1,10 +1,27 @@
 import { Col } from "react-bootstrap";
 
-export const ProjectCard = ({ title, description, imgUrl }) => {
+export const ProjectCard = ({ title, description, imgUrl, repoUrl }) => {
+  const handleClick = () => {
+    if (repoUrl) {
+      window.open(repoUrl, '_blank', 'noopener noreferrer');
+    }
+  };
+
   return (
     <Col size={12} sm={6} md={4}>
-      <div className="proj-imgbx">
-        <img src={imgUrl} />
+      <div 
+        className="proj-imgbx" 
+        onClick={handleClick}
+        style={{ cursor: repoUrl ? 'pointer' : 'default' }}
+        role="button"
+        tabIndex={0}
+        onKeyDown={(e) => {
+          if (e.key === 'Enter' || e.key === ' ') {
+            handleClick();
+          }
+        }}
+      >
+        <img src={imgUrl} alt={title} />
         <div className="proj-txtx">
           <h4>{title}</h4>
           <span>{description}</span>
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -7,18 +7,24 @@ import colorSharp2 from "../assets/img/color-sharp2.png";
 import 'animate.css';
 import TrackVisibility from 'react-on-screen';
 
+import projImg1_informatics from "../assets/img/projects/its-informatika-building.jpg";
+import projImg1_computerScience from "../assets/img/projects/computer-science-end.png";
+import projImg3_storeProfit from "../assets/img/projects/store-profit-visualization.png";
+
 export const Projects = () => {
 
   const projects = [
     {
-      title: "Data Project",
-      description: "Goals and descriptions",
-      imgUrl: projImg1,
+      title: "My Journey as a Computer Science Student to Become Data Scientist",
+      description: "- Blog -",
+      imgUrl: projImg1_informatics,
+      repoUrl: "https://medium.com/@davianoh/my-journey-as-a-computer-science-student-371447bd7820", 
     },
     {
-      title: "Data Project",
-      description: "Goals and descriptions",
-      imgUrl: projImg2,
+      title: "Learning Paths on Computer Science Major",
+      description: "- Blog -",
+      imgUrl: projImg1_computerScience,
+      repoUrl: "https://medium.com/@davianoh/learning-paths-on-computer-science-major-701fdfb15ef1", 
     },
     {
       title: "Data Project",
@@ -29,6 +35,45 @@ export const Projects = () => {
       title: "Data Project",
       description: "Goals and descriptions",
       imgUrl: projImg1,
+    },
+    {
+      title: "Data Project",
+      description: "Goals and descriptions",
+      imgUrl: projImg2,
+    },
+  ];
+
+  const projects2 = [
+    {
+      title: "My Journey as a Computer Science Student to Become Data Scientist",
+      description: "- Blog -",
+      imgUrl: projImg1_informatics,
+      repoUrl: "https://medium.com/@davianoh/my-journey-as-a-computer-science-student-371447bd7820", 
+    },
+    {
+      title: "Learning Paths on Computer Science Major",
+      description: "- Blog -",
+      imgUrl: projImg1_computerScience,
+      repoUrl: "https://medium.com/@davianoh/learning-paths-on-computer-science-major-701fdfb15ef1", 
+    },
+    {
+      title: "Data Project",
+      description: "Goals and descriptions",
+      imgUrl: projImg3,
+    },
+    {
+      title: "Data Project",
+      description: "Goals and descriptions",
+      imgUrl: projImg1,
+    }, 
+  ];
+
+  const projects3 = [
+    {
+      title: "Store's Profitability Overview Data Visualization",
+      description: "- Tableau -",
+      imgUrl: projImg3_storeProfit,
+      repoUrl: "https://public.tableau.com/app/profile/davian.benito/viz/Overview-SuperStore/ExecutiveOverview", 
     },
     {
       title: "Data Project",
@@ -55,13 +100,13 @@ export const Projects = () => {
                 <Tab.Container id="projects-tabs" defaultActiveKey="first">
                   <Nav variant="pills" className="nav-pills mb-5 justify-content-center align-items-center" id="pills-tab">
                     <Nav.Item>
-                      <Nav.Link eventKey="first">Blogs & Articles</Nav.Link>
+                      <Nav.Link eventKey="first" style={{ cursor: 'pointer' }}>Blogs & Articles</Nav.Link>
                     </Nav.Item>
                     <Nav.Item>
-                      <Nav.Link eventKey="second">Colab Notebooks</Nav.Link>
+                      <Nav.Link eventKey="second" style={{ cursor: 'pointer' }}>Colab Notebooks</Nav.Link>
                     </Nav.Item>
                     <Nav.Item>
-                      <Nav.Link eventKey="third">Deployed Projects</Nav.Link>
+                      <Nav.Link eventKey="third" style={{ cursor: 'pointer' }}>Deployed Projects</Nav.Link>
                     </Nav.Item>
                   </Nav>
                   <Tab.Content id="slideInUp" className={isVisible ? "animate__animated animate__slideInUp" : ""}>
@@ -80,10 +125,32 @@ export const Projects = () => {
                       </Row>
                     </Tab.Pane>
                     <Tab.Pane eventKey="second">
-                      <p>Google colabs notebooks</p>
+                      <Row>
+                        {
+                          projects2.map((project2, index2) => {
+                            return (
+                              <ProjectCard
+                                key={index2}
+                                {...project2}
+                                />
+                            )
+                          })
+                        }
+                      </Row>
                     </Tab.Pane>
                     <Tab.Pane eventKey="third">
-                      <p>Published projects, etc</p>
+                      <Row>
+                        {
+                          projects3.map((project3, index3) => {
+                            return (
+                              <ProjectCard
+                                key={index3}
+                                {...project3}
+                                />
+                            )
+                          })
+                        }
+                      </Row>
                     </Tab.Pane>
                   </Tab.Content>
                 </Tab.Container>
